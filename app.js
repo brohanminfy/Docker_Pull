34efac7cd5b5ed941c7f6d94e8ab801add9ef103
brohanminfy <boddupally.rohan@minfytech.com>
2025-05-19 13:55:19 +0530

    docker compose

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello Sai - From Samrath');
+  res.send('Hello Sai - From Rohan');
 });
 
 app.listen(PORT, () => {
